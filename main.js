f0c739bc1ddaf1638557dfdf02cbee6e0a95d413
James Rice <james@2b.co.uk>
2024-07-22 11:36:47 +0100

    fix typo

--- main.js
+++ main.js
@@ -1,73 +1,81 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const AdmZip = require('adm-zip');
-const filesize = require('filesize');
-const path = require('path');
-const fs = require('fs');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const AdmZip = require("adm-zip");
+const filesize = require("filesize");
+const path = require("path");
+const fs = require("fs");
 
 async function main() {
-  const token = core.getInput('github_token');
-  const pr = core.getInput('pr');
-  const artifactName = core.getInput('artifact_name', {required: true});
-  const workflowFilename = core.getInput('worfklow_file_name', {required: true});
-  const [owner, repo] = core.getInput('repo').split('/');
-  const destPath = core.getInput('path');
+  const token = core.getInput("github_token");
+  const pr = core.getInput("pr");
+  const artifactName = core.getInput("artifact_name", { required: true });
+  const workflowFilename = core.getInput("workflow_file_name", {
+    required: true,
+  });
+  const [owner, repo] = core.getInput("repo").split("/");
+  const destPath = core.getInput("path");
 
-  console.log('==> PR', pr);
-  console.log('==> Workflow Filename', workflowFilename);
-  console.log('==> Owner', owner);
-  console.log('==> Repo', repo);
+  console.log("==> PR", pr);
+  console.log("==> Workflow Filename", workflowFilename);
+  console.log("==> Owner", owner);
+  console.log("==> Repo", repo);
 
   const client = github.getOctokit(token);
 
   const pull = await client.rest.pulls.get({
     owner,
     repo,
-    pull_number: pr
+    pull_number: pr,
   });
 
   if (!pull) {
-    throw new Error('No PR found');
+    throw new Error("No PR found");
   }
 
-  console.log('==> Branch', pull.data.head.ref);
+  console.log("==> Branch", pull.data.head.ref);
 
   let artifacts = [];
 
-  for await (const runs of client.paginate.iterator(client.rest.actions.listWorkflowRuns, {
-    owner,
-    repo,
-    workflow_id: workflowFilename,
-    event: 'pull_request',
-    status: 'success',
-    branch: pull.data.head.ref
-  })) {
+  for await (const runs of client.paginate.iterator(
+    client.rest.actions.listWorkflowRuns,
+    {
+      owner,
+      repo,
+      workflow_id: workflowFilename,
+      event: "pull_request",
+      status: "success",
+      branch: pull.data.head.ref,
+    }
+  )) {
     for (const run of runs.data) {
-      let af = await client.paginate(client.rest.actions.listWorkflowRunArtifacts, {
-        owner: owner,
-        repo: repo,
-        run_id: run.id,
-      });
+      let af = await client.paginate(
+        client.rest.actions.listWorkflowRunArtifacts,
+        {
+          owner: owner,
+          repo: repo,
+          run_id: run.id,
+        }
+      );
 
-      artifacts.push(...af.filter(a => a.name === artifactName));
+      artifacts.push(...af.filter((a) => a.name === artifactName));
     }
   }
 
   artifacts = artifacts.filter(Boolean);
 
   if (artifacts.length === 0) {
-    throw new Error('No artifacts found');
+    throw new Error("No artifacts found");
   }
 
-  console.log('==> Artifacts found', artifacts.map(a => a.id).join(', '));
+  console.log("==> Artifacts found", artifacts.map((a) => a.id).join(", "));
 
   // sort by date DESC
-  artifacts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+  artifacts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
   // get latest created artifact
   const artifact = artifacts[0];
 
-  console.log('==> Chosen Artifact', artifact.id);
+  console.log("==> Chosen Artifact", artifact.id);
   const size = filesize(artifact.size_in_bytes, { base: 10 });
 
   console.log(`==> Downloading: ${artifact.name}.zip (${size})`);
